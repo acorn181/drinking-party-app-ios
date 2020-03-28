fea559946d7267681ed21d0be10d93d8265f85e8
acorn181 <masa.ichi.181@gmail.com>
2020-03-28 12:48:55 +0900

    first commit.

---
+++ DrinkingPlace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrinkingPlace
+//
+//  Created by Masahiro Ichikawa on 2020/03/28.
+//  Copyright © 2020 Isobe Gundan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
